753dc43f688de720313fb6ca8a292ab2e06a56f4
xingqiba <ixqbar@gmail.com>
2017-07-03 14:15:16 +0800

    update readme

--- example/test_jieba.php
+++ example/test_jieba.php
@@ -2,14 +2,13 @@
 
 $result = jieba('小明硕士毕业于中国科学院计算所，后在日本京都大学深造');
 echo implode('/', $result) . PHP_EOL;
+//计算所/小明/京都大学/深造/硕士/中国科学院/毕业/日本
 
+$result = jieba('小明硕士毕业于中国科学院计算所，后在日本京都大学深造', 1, 50);
+echo implode('/', $result) . PHP_EOL;
 //小明/硕士/毕业/于/中国/科学/学院/科学院/中国科学院/计算/计算所/，/后/在/日本/京都/大学/京都大学/深造
 
-$result = jieba('小明硕士毕业于中国科学院计算所，后在日本京都大学深造', 1, 6);
-echo implode('/', $result) . PHP_EOL;
-//计算所/小明/京都大学/深造/硕士/中国科学院
-
-$result = jieba('他心理健康');
+$result = jieba('他心理健康', 1);
 echo implode('/', $result) . PHP_EOL;
 //他/心理/健康/心理健康
 
